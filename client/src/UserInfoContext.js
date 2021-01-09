a234706a10ab00a0f0e6cfc0d74bc35ca1aef7eb
Mae <maeberris@outlook.com>
2021-01-09 10:39:46 -0500

    react set up

---
+++ client/src/UserInfoContext.js
@@ -0,0 +1,15 @@
+import React from "react";
+import useStickyState from "./useLocalStorageHook";
+
+export const UserInfoContext = React.createContext(null);
+
+export const UserInfoContextProvider = ({ children }) => {
+  const [userName, setUserName] = useStickyState("", "userName");
+  const [test, setTest] = React.useState("testing");
+
+  return (
+    <UserInfoContext.Provider value={{ userName, test }}>
+      {children}
+    </UserInfoContext.Provider>
+  );
+};

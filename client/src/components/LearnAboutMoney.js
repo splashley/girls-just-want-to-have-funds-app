7c720064594563292d06bd3447f878882b0fad51
Mae <maeberris@outlook.com>
2021-01-09 20:15:38 -0500

    bubble fix

--- client/src/components/LearnAboutMoney.js
+++ client/src/components/LearnAboutMoney.js
@@ -15,28 +15,26 @@ const LearnAboutMoneyPage = () => {
     <Wrapper>
       <Logo />
       <SubHeader>LET'S LEARN ABOUT MONEY {userName.toUpperCase()} !</SubHeader>
-      <GridWrapper>
-        <ParagraphWrapper>
-          <Paragraph>
-            <strong>Savings:</strong> Let's say your parents give you a 10
-            dollar allowance every week for doing the chores around the house.
-            Those 10 dollars are what we call your <strong>income</strong>. Now,
-            you've had your eye on the new Feline Festival game and you've been
-            putting aside your allowance for it. That money you've put aside is
-            your <strong>savings</strong>.
-          </Paragraph>
-        </ParagraphWrapper>
-        <AvatarSection>
-          <AvatarSideImg src={AvatarImg} />
-
-          <Bubble>
-            <Tip />
-            My <strong>income</strong> is 15 bucks a week. I have been saving
-            for two weeks so I have 30 dollars in <strong>savings</strong> YAAY
-            !
-          </Bubble>
-        </AvatarSection>
-      </GridWrapper>
+      {/* <GridWrapper> */}
+      <ParagraphWrapper>
+        <Paragraph>
+          <strong>Savings:</strong> Let's say your parents give you a 10 dollar
+          allowance every week for doing the chores around the house. Those 10
+          dollars are what we call your <strong>income</strong>. Now, you've had
+          your eye on the new Feline Festival game and you've been putting aside
+          your allowance for it. That money you've put aside is your{" "}
+          <strong>savings</strong>.
+        </Paragraph>
+      </ParagraphWrapper>
+      <AvatarSection>
+        <AvatarSideImg src={AvatarImg} />
+        <Bubble>
+          <Tip />
+          My <strong>income</strong> is 15 bucks a week. I have been saving for
+          two weeks so I have 30 dollars in <strong>savings</strong> YAAY !
+        </Bubble>
+      </AvatarSection>
+      {/* </GridWrapper> */}
       <ButtonWrapper nextLink="/settinggoalpage" />
     </Wrapper>
   );
@@ -71,9 +69,11 @@ const Paragraph = styled.p`
 `;
 
 const AvatarSection = styled.div`
+  width: fit-content;
+  height: fit-content;
   position: relative;
-  text-align: center;
 `;
+
 const AvatarSideImg = styled.img`
   padding: 5px 5px 25px 5px;
   height: 400px;
@@ -85,11 +85,11 @@ const Bubble = styled.div`
   background: white;
   font-size: 20px;
   padding: 15px;
-  max-width: 280px;
   position: absolute;
-  top: 30%;
-  left: 850px;
+  top: 34%;
+  left: 93px;
   z-index: 1;
+  width: 300px;
 `;
 
 const Tip = styled.div`
